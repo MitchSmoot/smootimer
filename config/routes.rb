4d1d6a694ec993a6fe1e96fcb4af19bffd5e9056
Mitch Smoot <mitchsmoot@gmail.com>
2018-10-09 23:16:41 -0600

    created routes for User page

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users do
     resources :single_solves
+  end
 end
